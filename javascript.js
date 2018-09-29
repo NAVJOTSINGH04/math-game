2436b363cc01f99352303d5d2a0073e1c2338a92
Navjot Singh <singhnavjot025@gmail.com>
2018-09-30 02:34:16 +0530

    game over box

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 var playing = false;
 var score;
+var action;
+var timeremaining;
 //if we click on the start/reset
 document.getElementById("startreset").onclick = function () {
     //if we are playing
@@ -18,10 +20,54 @@ document.getElementById("startreset").onclick = function () {
         
         
         //show countdown box
-     document.getElementById("time").style.display = "block"; 
+    show("time");
+        timeremaining = 60;
+        document.getElementById("timevalue").innerHTML = timeremaining;
+        // to hide game over
+        hide("gameOver");
         
         //change button  to reset
-        document.getElementById("startreset").innerHTML="reset game"
+        document.getElementById("startreset").innerHTML="reset game";
+        
+        
+        
+        startcountdown();
+        
     }
-    
+}
+
+
+
+//function
+
+//start count down
+function startcountdown(){
+    action = setInterval(function(){
+       timeremaining -= 1; 
+         document.getElementById("timevalue").innerHTML = timeremaining;
+        if( timeremaining == 0){
+            stopcountdown();
+            show("gameOver");
+            document.getElementById("gameOver").innerHTML= "<p>game over</p><p>your score is" + score +"</p>";
+            
+            hide("time");
+            hide("correct");
+             hide("wrong");
+            playing = false;
+            document.getElementById("startreset").innerHTML = "Start Game";
+            //hide("gameOver");
+        }
+    }, 1000);
+}
+// stop count down
+function stopcountdown(){
+    clearInterval(action);
+}
+//hide certain elements
+function hide (Id){
+    document.getElementById(Id).style.display = "none";
+}
+//show certain element 
+function show (Id){
+    document.getElementById(Id).style.display = "block";
 }
